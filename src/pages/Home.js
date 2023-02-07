3cd72e01a28b93f2924aa40d1b59683ac51a44d8
MariaCristina <mariacristinasanchezdelacruz@gmail.com>
2023-02-06 23:59:22 -0600

    Fetch added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Navbar } from '../components/Navbar'
 
 export const Home = () => {
-  return (
-    <>
-        <Navbar/>
-        <div>Home</div>
-    </>
-  )
+    const [users, setUsers] = useState([])
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch("https://randomuser.me/api/?results=10");
+            const jsonData = await response.json();
+            setUsers(jsonData.results);
+        }
+        fetchData();
+    }, []);
+    
+    return (
+        <>
+            <Navbar />
+            <div>Users</div>
+            <div className='d-flex flex-wrap'>
+                {
+                    users?.map(user => (
+                        <div key={user.id.value}  className="card mx-2 my-2" style={{width: "18rem"}}>
+                            <img src={user.picture.medium} className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">{ user.name.title + " " + user.name.first + " " + user.name.last}</h5>
+                                    <p className="card-text">{"Gender: " + user.gender}</p>
+                                    <p className="card-text">{"Phone: " + user.phone}</p>
+                                    <p className="card-text">{"Email: " + user.email}</p>
+                                </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </>
+    )
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,24 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+
+const userInit = {
+    email : "",
+    password: ""
+}
 
 export const Login = () => { 
     const navigate = useNavigate();
+    const [user, setUser] = useState(userInit)
     const handleSubmit = (e) =>{
         e.preventDefault();
         navigate("/Home");
     }
+    const handleChange = (e) => {
+        e.preventDefault();
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        })
+    }
     return (
         <div className="d-flex flex-row justify-content-center align-self-center" style={{ padding: "10rem" }}>
             <div className="card  shadow " style={{ width: "18rem" }}>
-                <form className='my-3 mx-3' onSubmit={handleSubmit}>
+                <form className='my-3 mx-3' onSubmit={handleSubmit} autoComplete="off">
                     <div className="mb-3">
                         <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required/>
+                        <input 
+                            type="email" 
+                            className="form-control" 
+                            aria-describedby="emailHelp" 
+                            required
+                            value={user.email}
+                            onChange={handleChange}
+                            name="email"
+                            autoComplete='off'
+                        />
                         <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1" required />
+                        <input 
+                            type="password" 
+                            className="form-control" 
+                            required 
+                            value={user.password}  
+                            onChange={handleChange} 
+                            name='password' 
+                            autoComplete='off'
+                        />
                     </div>
                     <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
