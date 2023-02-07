d667044187efea4de92673533527f9454ba532c1
MariaCristina <mariacristinasanchezdelacruz@gmail.com>
2023-02-06 22:33:50 -0600

    added loging functionallity

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,22 +1,24 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
-export const Login = () => {
+export const Login = () => { 
+    const navigate = useNavigate();
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        navigate("/Home");
+    }
     return (
         <div className="d-flex flex-row justify-content-center align-self-center" style={{ padding: "10rem" }}>
             <div className="card  shadow " style={{ width: "18rem" }}>
-                <form className='my-3 mx-3'>
+                <form className='my-3 mx-3' onSubmit={handleSubmit}>
                     <div className="mb-3">
                         <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required/>
                         <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1" />
-                    </div>
-                    <div className="mb-3 form-check">
-                        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                        <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1" required />
                     </div>
                     <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
